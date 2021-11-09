5f406eb2363de745a43c76cc301e4cffd9423e7c
izales <i.zales@montary-agency.de>
2021-11-09 13:32:18 +0100

    changes

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = ({ children }: ILayoutProps): JSX.Element => {
       <h5>Footer Content</h5>
       {children}
       <script async src="https://cdn.snipcart.com/themes/v3.0.21/default/snipcart.js" />
-<div hidden id="snipcart" data-api-key="MmI3MjMyNzItZWVhNi00NTJlLTg4ODMtZmZkNTJmZTczMmQ4NjM3NzEwMjQ1MTk3ODA3Mjgx" />
+<div hidden id="snipcart" data-api-key="ST_Y2IzNGI3NWMtMzk0Yy00NTVlLWE0MzctZGZkZjViODhjOTNlNjM3NzIwNTc0Mzk2NTgyMDQ2" />
     </footer>
   )
 }
